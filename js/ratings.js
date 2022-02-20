755b3c15db40dc37723947f6dca3cb4784b5e120
sandra dib <sandra_dib_1998@hotmail.com>
2022-02-20 17:18:07 +0100

    Completed module

--- js/ratings.js
+++ js/ratings.js
@@ -1,9 +1,9 @@
-function collect_ratings() {
+const collect_ratings = () => {
 
-  const ratings {
-    count: 0,
-    sum: 0,
-    average: 0
+  const ratings = {
+    'count': 0,
+    'sum': 0,
+    'average': 0
   };
 
   let rating =0;
